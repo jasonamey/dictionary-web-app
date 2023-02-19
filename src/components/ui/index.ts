b596a04716f2d2aca9cdad61e502dcf114f7182f
Jason Amey <jason.amey@gmail.com>
2023-02-19 17:47:16 -0500

    clean up removing files

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,2 @@
 export { ContentContainer } from './content-container'
 export { PageContainer } from './page-container'
-export { ToggleSwitch } from './toggle-switch'
-export { Loader } from './loader'
