8ed1a77bd4d8c55c632e2414b49d4b2d2eecca4a
Jason Amey <jason.amey@gmail.com>
2023-03-18 16:36:40 -0400

    refactor api response utils

--- src/api/utils/index.ts
+++ src/api/utils/index.ts
@@ -17,6 +17,63 @@ import {
   EXAMPLE,
 } from '@/constants'
 
+/*
+
+RESPONSE FROM DICTIONARY API : 
+
+interface Response {
+  word: string
+  phonetic: string
+  phonetics: Phonetic[]
+  origin: string
+  meanings: ResponseMeaning[]
+  sourceUrls: string[]
+}
+
+interface ResponseMeaning {
+  partOfSpeech: string
+  definitions: ResponseDefinition[]
+  synonyms?: string[]
+}
+
+interface ResponseDefinition {
+  definition: string
+  example?: string
+  synonyms: unknown[]
+  antonyms: unknown[]
+}
+
+interface Phonetic {
+  text: string
+  audio?: string
+}
+
+************************
+
+FINAL SHAPE OF DATA FOR DISPLAY IN APP: 
+
+type AppDefinitionType = {
+  definition: string
+  example?: string
+}
+
+type AppFormattedMeaningType = {
+  partOfSpeech: string
+  synonyms: string[] | undefined
+  definitions: AppDefinitionType[]
+}
+
+type AppDictionaryDefinitionType = {
+  word: string
+  audio: string
+  text: string
+  meanings: AppFormattedMeaningType[]
+  sourceUrl: string
+}
+
+*/
+
+//check if both phonetic and audio of phonetic exists
 function getPhonetics(phonetics: Phonetic[]) {
   for (const item of phonetics) {
     if (
@@ -29,6 +86,7 @@ function getPhonetics(phonetics: Phonetic[]) {
   }
 }
 
+//function to remove ':' at end of definition
 function cleanDefinition(definition: string) {
   if (definition.slice(definition.length - 1) === ':') {
     return `${definition.slice(0, definition.length - 1)}.`
@@ -37,6 +95,22 @@ function cleanDefinition(definition: string) {
   }
 }
 
+//reshape ResponseMeaning into AppDefinitionType[]
+function getDefinitions(meaning: ResponseMeaning) {
+  const definitions: AppDefinitionType[] = []
+  meaning[DEFINITIONS].forEach((definitionItem) => {
+    const newDefinitionItem = {
+      definition: cleanDefinition(definitionItem[DEFINITION]),
+    }
+    if (Object.prototype.hasOwnProperty.call(definitionItem, EXAMPLE)) {
+      newDefinitionItem[EXAMPLE] = definitionItem[EXAMPLE]
+    }
+    definitions.push(newDefinitionItem)
+  })
+  return definitions
+}
+
+//reshape ResponseMeaning[] into AppFormattedMeaningType[]
 function getMeanings(meanings: ResponseMeaning[]) {
   let formattedMeanings: AppFormattedMeaningType[] = []
   meanings.forEach((meaningItem) => {
@@ -44,22 +118,13 @@ function getMeanings(meanings: ResponseMeaning[]) {
     meaningToAdd[PART_OF_SPEECH] = meaningItem[PART_OF_SPEECH]
     meaningToAdd[SYNONYMS] =
       meaningItem[SYNONYMS] === undefined ? [] : meaningItem[SYNONYMS]
-    const definitions: AppDefinitionType[] = []
-    meaningItem[DEFINITIONS].forEach((definitionItem) => {
-      const newDefinitionItem = {
-        definition: cleanDefinition(definitionItem[DEFINITION]),
-      }
-      if (Object.prototype.hasOwnProperty.call(definitionItem, EXAMPLE)) {
-        newDefinitionItem[EXAMPLE] = definitionItem[EXAMPLE]
-      }
-      definitions.push(newDefinitionItem)
-    })
-    meaningToAdd[DEFINITIONS] = definitions
+    meaningToAdd[DEFINITIONS] = getDefinitions(meaningItem)
     formattedMeanings = [...formattedMeanings, meaningToAdd]
   })
   return formattedMeanings
 }
 
+//takes API response and reshapes into AppDictionaryDefinitionType for final display
 export function shapeApiResponse(jsonResponse: Response) {
   const wordContext: AppDictionaryDefinitionType =
     {} as AppDictionaryDefinitionType
